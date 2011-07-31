44472e5f65e8fe2204d96f40ea8d4d043d06c7f4
Tony Rasa <trasa@meancat.com>
2011-07-31 15:38:24 -0700

    Initial skeleton

---
+++ src/zone-server/src/main/java/BronzeThistle/App.java
@@ -0,0 +1,13 @@
+package BronzeThistle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
