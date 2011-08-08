03b059928255da9c43862aaa9fed26c9c05dc593
Tony Rasa <trasa@meancat.com>
2011-08-08 14:43:08 -0700

    removed dead code
    removed some while(true) branches that were never necessary and a
    bunch of code from the original console client app that dealt with
    reading input from stdin.

--- src/client/src/main/java/bronzethistle/client/App.java
+++ src/client/src/main/java/bronzethistle/client/App.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.core.io.Resource;
@@ -20,8 +19,6 @@ import org.springframework.util.Log4jConfigurer;
 import org.springframework.util.SystemPropertyUtils;
 
 import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,8 +34,6 @@ public class App {
             "file:${bronzethistle-client.home}/conf/bronzethistle-client.properties"
     };
 
-    private AbstractApplicationContext applicationContext;
-
     @Option(name = "-h", aliases = {"--home"}, usage = "The home directory of the application.", required = true)
     private String homePath;
 
@@ -50,17 +45,13 @@ public class App {
         commandLineParser.parseArgument(args);
 
         instance.start();
-        // TODO remove this, it isnt needed (i think)
-        while(true) {
-            Thread.sleep(1000);
-        }
     }
 
     private void start() throws Exception {
         System.setProperty("bronzethistle-client.home", homePath);
         Log4jConfigurer.initLogging("file:${bronzethistle-client.home}/conf/log4j.xml");
 
-        applicationContext = new AnnotationConfigApplicationContext();
+        AbstractApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         ((AnnotationConfigApplicationContext) applicationContext).scan(APPLICATION_PACKAGES);
 
         registerPropertyConfigurer(applicationContext);
@@ -77,48 +68,6 @@ public class App {
         frame.pack();
         frame.setVisible(true);
         frame.setSize(800, 600);
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        in.readLine();
-
-//  for (; ; ) {
-//            String line = in.readLine();
-//            if (line == null) {
-//                break;
-
-//        Channel channel = applicationContext.getBean(Channel.class);
-
-//        // Read commands from the stdin.
-//        ChannelFuture lastWriteFuture = null;
-//        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-//        for (; ; ) {
-//            String line = in.readLine();
-//            if (line == null) {
-//                break;
-//            }
-//
-//            // Sends the received line to the server.
-//            log.info("sending " + line);
-//            lastWriteFuture = channel.write(new SerializedClientMessage(line));
-//
-//            // If user typed the 'bye' command, wait until the server closes
-//            // the connection.
-//            if (line.toLowerCase().equals("bye")) {
-//                channel.getCloseFuture().awaitUninterruptibly();
-//                break;
-//            }
-//        }
-
-//        // Wait until all messages are flushed before closing the channel.
-//        if (lastWriteFuture != null) {
-//            lastWriteFuture.awaitUninterruptibly();
-//        }
-
-        // Close the connection.  Make sure the close operation ends because
-        // all I/O operations are asynchronous in Netty.
-//        channel.close().awaitUninterruptibly();
-//
-//        // Shut down all thread pools to exit.
-//        bootstrap.releaseExternalResources();
     }
 
 
@@ -146,9 +95,5 @@ public class App {
                 ((DefaultListableBeanFactory) beanFactory).registerBeanDefinition("propertyConfigurer", definition);
             }
         });
-    }
-
-     public final ApplicationContext getApplicationContext() {
-        return applicationContext;
     }
 }
