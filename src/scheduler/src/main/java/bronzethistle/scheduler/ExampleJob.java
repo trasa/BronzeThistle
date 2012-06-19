515f0d6473da4745a3e4d1d64baa7f41c6893dac
Tony Rasa <trasa@meancat.com>
2012-06-18 17:38:57 -0700

    working example scheduler

--- src/scheduler/src/main/java/bronzethistle/scheduler/ExampleJob.java
+++ src/scheduler/src/main/java/bronzethistle/scheduler/ExampleJob.java
@@ -6,13 +6,20 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Service
 public class ExampleJob {
     private static final Logger log = LoggerFactory.getLogger(ExampleJob.class);
 
-    @Scheduled(fixedDelay = 1000L)
+    private AtomicInteger i = new AtomicInteger();
+
+    @Scheduled(fixedRate = 3000L)
     public void run() {
-        System.out.println("running");
-        log.info("running - {}", System.currentTimeMillis());
+        log.info("running - {} - {}", i.getAndIncrement(), new Date());
+        try {
+            Thread.sleep(4000L);
+        } catch (InterruptedException ignore) {}
     }
 }
