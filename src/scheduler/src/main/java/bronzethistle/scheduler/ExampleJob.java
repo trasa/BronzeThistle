0edb6fbed17a2f4383bc2224474d6953e5f2c010
Tony Rasa <trasa@meancat.com>
2012-06-19 17:15:32 -0700

    enhanced example job to show what happens when it falls behind

--- src/scheduler/src/main/java/bronzethistle/scheduler/ExampleJob.java
+++ src/scheduler/src/main/java/bronzethistle/scheduler/ExampleJob.java
@@ -8,15 +8,29 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class ExampleJob {
     private static final Logger log = LoggerFactory.getLogger(ExampleJob.class);
 
     private AtomicInteger i = new AtomicInteger();
+    private AtomicLong lastRun = new AtomicLong();
 
     @Scheduled(fixedRate = 3000L)
     public void run() {
+        long timeSinceLastRun = System.currentTimeMillis() - lastRun.get();
+        if (timeSinceLastRun > 3000) {
+            log.warn("Scheduled job is falling behind!! Time since last run: {}", timeSinceLastRun);
+        }
+        lastRun.set(System.currentTimeMillis());
+        // this is where you'd have "stuff happen" ..
+        // this is just sleeping and log output for testing purposes...
+        doWork();
+    }
+
+
+    private void doWork() {
         log.info("running - {} - {}", i.getAndIncrement(), new Date());
         try {
             Thread.sleep(4000L);
